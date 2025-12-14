dfc6b613d1cbb32d83ec02ccaf78f413431cb767
kuromon11 <yoshikikuro712@gmail.com>
2025-12-14 16:15:42 +0900

    feat: #4 ViewSetを作成

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+from .models import Deck, Card, StudyLog
+from .serializers import (
+    DeckSerializer,
+    CardSerializer,
+    StudyLogSerializer,
+)
 
-# Create your views here.
+
+class DeckViewSet(ModelViewSet):
+    queryset = Deck.objects.all()
+    serializer_class = DeckSerializer
+
+
+class CardViewSet(ModelViewSet):
+    queryset = Card.objects.all()
+    serializer_class = CardSerializer
+
+
+class StudyLogViewSet(ModelViewSet):
+    queryset = StudyLog.objects.all()
+    serializer_class = StudyLogSerializer
