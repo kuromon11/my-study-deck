2892fa6179825ad9409f542dd8748f31d90af6f7
kuromon11 <yoshikikuro712@gmail.com>
2025-12-17 00:53:37 +0900

    feat: #5 commonのCSSファイルを用意し、各ファイルで適用されるようにする

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -3,5 +3,6 @@ import './style.css';
 import App from './App.vue';
 import router from './router';
 import { vuetify } from './plugins/vuetify';
+import './common.css';
 
 createApp(App).use(vuetify).use(router).mount('#app');
