800352d2a25dc215ce1b034c17681c7a124e8e1b
kuromon11 <yoshikikuro712@gmail.com>
2025-12-23 23:06:08 +0900

    feat: #10 学習画面用のカードの型定義を実装

--- frontend/src/interfaces/card.ts
+++ frontend/src/interfaces/card.ts
@@ -10,3 +10,16 @@ export interface CardForm {
   question: string;
   answer: string;
 }
+
+export interface CardWithLogs {
+  id: number;
+  question: string;
+  answer: string;
+  study_logs: {
+    id: number;
+    studied_at: string;
+    is_correct: boolean;
+  };
+  created_at: string;
+  updated_at: string;
+}
