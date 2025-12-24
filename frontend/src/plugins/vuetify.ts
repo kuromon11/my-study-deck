9584320e2e57675559a1f5d188f8f6ea3f01a341
kuromon11 <yoshikikuro712@gmail.com>
2025-12-24 18:17:25 +0900

    chore: #20 Vuetifyの日本語化

--- frontend/src/plugins/vuetify.ts
+++ frontend/src/plugins/vuetify.ts
@@ -2,6 +2,7 @@ import 'vuetify/styles';
 import { createVuetify } from 'vuetify';
 import '@mdi/font/css/materialdesignicons.css';
 import type { ThemeDefinition } from 'vuetify';
+import { ja } from 'vuetify/locale';
 
 const lightTheme: ThemeDefinition = {
   dark: false,
@@ -11,6 +12,11 @@ const lightTheme: ThemeDefinition = {
 };
 
 export const vuetify = createVuetify({
+  locale: {
+    locale: 'ja',
+    fallback: 'en',
+    messages: { ja },
+  },
   theme: {
     defaultTheme: 'light',
     themes: {
